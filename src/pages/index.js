9b379c54c08fb5570bbd9a1831d6b111d4889d11
Shunichi Hasegawa <shsgwxidjs@gmail.com>
2018-08-05 00:24:23 +0900

    commit test

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <Link to="/page-2/">Watch the videos</Link>
+  </div>
+)
+
+export default IndexPage
